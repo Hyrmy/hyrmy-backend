f3b4ad88ae65d306dca7625321823e8f5a1d397a
35844 <olli.ojala@hotmail.com>
2020-10-22 20:00:24 +0300

    Use express

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const app = require('./app') // varsinainen Express-sovellus
 const http = require('http')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
+const express = require('express')
+
 app.use(express.static('build'))
 
 const server = http.createServer(app)
